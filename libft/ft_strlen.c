03866c9e74a650ef99c8027e1d20790934fe71d0
Maria Luiza Rabelo Serpa <mrabelo-@c4b4c3.42berlin.de>
2023-11-13 18:04:52 +0100

    Add project libft

---
+++ libft/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 15:27:26 by mrabelo-          #+#    #+#             */
+/*   Updated: 2023/11/13 17:20:39 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+size_t	ft_strlen(const char*s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/*#include <stdio.h>
+
+int	main()
+{
+	int	test1 = ft_strlen("helloworld");
+	
+	printf("%i\n", test1);
+}*/
