fc4ad5f60c0dd13d925af13dbcc7f7c82522e925
Maria Luiza Rabelo Serpa <mrabelo-@c4b4c1.42berlin.de>
2023-11-14 17:32:53 +0100

    Add more functions

---
+++ libft/ft_memset.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 15:45:38 by mrabelo-          #+#    #+#             */
+/*   Updated: 2023/11/14 16:15:36 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memset(void*s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*p;
+
+	i = 0;
+	p = (unsigned char *)s;
+	while (i < n)
+	{
+		*p = (unsigned char)c;
+		i++;
+		p++;
+	}
+	return (s);
+}
+
+/*#include <stdio.h>
+
+int	main() 
+{
+    char str[20];
+    int fillValue = 'A';
+    size_t numChars = 10;
+
+    ft_memset(str, fillValue, numChars);
+    printf("Filled string: %s\n", str);
+
+    return (0);
+}*/
---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strlcpy.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 16:22:16 by mrabelo-          #+#    #+#             */
+/*   Updated: 2023/11/14 17:08:46 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+size_t	strlcpy(char*dest, const char*src, size_t n)
+{
+	size_t	i;
+	size_t	len;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	i = 0;
+	if (n != 0)
+	{
+		while (src[i] != '\0' && i < (n - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (len);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char test[] = "Hello, World";
+	char dest[50];
+	printf("%d\n", ft_strlcpy(dest, test, 7));
+	printf("%s\n", dest);
+}
