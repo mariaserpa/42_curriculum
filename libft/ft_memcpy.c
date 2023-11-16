ef4771fb5e91a5a68e857b979cbf863f092bfe42
Maria Luiza Rabelo Serpa <mrabelo-@c4b3c5.42berlin.de>
2023-11-16 18:26:11 +0100

    Fix norminette on ft_bzero, ft_memcpy, ft_memmove

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 14:36:56 by mrabelo-          #+#    #+#             */
+/*   Updated: 2023/11/16 14:57:31 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <string.h>
 
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*temp_dest;
 	unsigned char	*temp_src;
 
--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,73 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 15:15:40 by mrabelo-          #+#    #+#             */
+/*   Updated: 2023/11/16 16:26:04 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
-void    *ft_memmove(void*dest, const void*src, size_t len)
+void	*ft_memmove(void*dest, const void*src, size_t len)
 {
-        size_t  i;
-        unsigned char   *temp_dest;
-        unsigned char   *temp_src;
+	size_t			i;
+	unsigned char	*temp_dest;
+	unsigned char	*temp_src;
 
-        i = 0;
-        temp_dest = (unsigned char *)dest;
-        temp_src = (unsigned char *)src;
-        if (!temp_dest && !temp_src)
-                return (0);
-        if (temp_src < temp_dest)
-        {
-                i = len;
-                while (i > 0)
-                {
-                        i--;
-                        temp_dest[i] = temp_src[i];
-                }
-        }
-        else
-        {
-                i = 0;
-                while (i < len)
-                {
-                        temp_dest[i] = temp_src[i];
-                        i++;
-                }
-        }
-        return (dest);
+	i = 0;
+	temp_dest = (unsigned char *)dest;
+	temp_src = (unsigned char *)src;
+	if (!temp_dest && !temp_src)
+		return (0);
+	if (temp_src < temp_dest)
+	{
+		i = len;
+		while (i-- > 0)
+			temp_dest[i] = temp_src[i];
+	}
+	else
+	{
+		while (i < len)
+		{
+			temp_dest[i] = temp_src[i];
+			i++;
+		}
+	}
+	return (dest);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 
-int	main() {
-    // Test Scenario 1: Overlapping memory regions
-    char str1[] = "Hello, World!";
-    char buffer1[20];
+int	main()
+{
+	// Test 1: Overlapping memory regions
+	char str1[] = "Hello, World!";
+	char buffer1[20];
 
-    // Move the contents of str1 to buffer1 (overlapping)
-    ft_memmove(buffer1, str1 + 6, 8);
+	ft_memmove(buffer1, str1 + 6, 8);
+	printf("Scenario 1 - Overlapping: %s\n", buffer1);
 
-    // Display the result
-    printf("Scenario 1 - Overlapping: %s\n", buffer1);
+	// Test 2: Non-overlapping memory regions
+	char str2[] = "Hello, World!";
+	char buffer2[20];
 
-    // Test Scenario 2: Non-overlapping memory regions
-    char str2[] = "Hello, World!";
-    char buffer2[20];
+	ft_memmove(buffer2, str2, 8);
+	printf("iScenario 2 - Non-overlapping: %s\n", buffer2);
 
-    // Move the contents of str2 to buffer2 (non-overlapping)
-    ft_memmove(buffer2, str2, 8);
+	// Test 3: src is bigger than dst
+	char src[] = "This is a larger string";
+	char dst[10];
 
-    // Display the result
-    printf("Scenario 2 - Non-overlapping: %s\n", buffer2);
-
-	// Test Scenario: src is bigger than dst
-    char src[] = "This is a larger string";
-    char dst[10];
-
-    // Display the initial state of dst
-    printf("Before memmove: %s\n", dst);
-
-    // Use memmove to copy the content of src to dst
-    ft_memmove(dst, src, sizeof(src));
-
-    // Display the result
-    printf("After memmove: %s\n", dst);
-
-    return (0);
-}
-
+	printf("Before memmove: %s\n", dst);
+	ft_memmove(dst, src, 9);
+	printf("After memmove: %s\n", dst);
+	return (0);
+}*/
