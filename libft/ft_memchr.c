8045540319707fa7bc6501cde95a32fe3785d647
mariaserpa <iza-serpa@hotmail.com>
2023-11-16 22:01:03 +0100

    Add ft_memchr and ft_strrchr. Reminder: still needs to insert header

---
+++ libft/ft_memchr.c
@@ -0,0 +1,40 @@
+
+#include <string.h>
+
+void	*ft_memchr(const void*s, int c, size_t n)
+{
+	size_t		i;
+	unsigned char	temp_c;
+	unsigned char	*temp_s;
+
+	i = 0;
+	temp_c = (unsigned char)c;
+	temp_s = (unsigned char *)s;
+	while (i < n)
+	{
+		if (temp_s[i] == temp_c)
+			return (temp_s + i);
+		i++;
+	}
+	return (0);
+}
+
+/*#include <stdio.h>
+
+int main() {
+	const char *str = "Hello, world!";
+	int target_char = 'o';
+	size_t	n = 7;
+	char *result = ft_memchr(str, target_char, n);
+
+	if (result != 0)
+	{
+		printf("Found character '%c' at position: %ld\n", target_char, result - str);
+	}
+	else
+	{
+		printf("Character '%c' not found in the string.\n", target_char);
+	}
+
+	return (0);
+}*/
---
+++ libft/ft_strrchr.c
@@ -0,0 +1,41 @@
+
+char	*ft_strrchr(const char*s, int c)
+{
+	int		i;
+	char	temp_c;
+	char	*temp_s;
+
+	i = 0;
+	temp_c = (char)c;
+	temp_s = (char *)s;
+	while (temp_s[i] != '\0')
+                i++;
+	if (temp_s[i] == '\0' && temp_c == '\0')
+		return (temp_s + i);
+	while (i >= 0)
+	{
+		if (temp_s[i] == temp_c)
+			return (temp_s + i);
+		i--;
+	}
+	return (0);
+}
+
+/*#include <stdio.h>
+
+int main() {
+	const char *str = "Hello, world!";
+	int target_char = 'o';
+	char *result = ft_strrchr(str, target_char);
+
+	if (result != 0)
+	{
+		printf("Found character '%c' at position: %ld\n", target_char, result - str);
+	}
+	else
+	{
+		printf("Character '%c' not found in the string.\n", target_char);
+	}
+
+	return (0);
+}*/
