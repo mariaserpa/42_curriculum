dd193ad0a1605af6eea7779bbe344a8bfce14584
Maria Luiza Rabelo Serpa <mrabelo-@c4b4c4.42berlin.de>
2023-11-17 16:02:41 +0100

    Add ft_atoi and ft_strnstr

---
+++ libft/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 15:25:16 by mrabelo-          #+#    #+#             */
+/*   Updated: 2023/11/17 16:01:44 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char*str)
+{
+	int	i;
+	int	signal;
+	int	number;
+
+	i = 0;
+	signal = 1;
+	number = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signal = -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57 && str[i] != '\0')
+	{
+		number = number * 10;
+		number += str[i] - 48;
+		i++;
+	}
+	return (signal * number);
+}
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char s[] = "    -1234ab567";
+
+	printf("%d\n", ft_atoi(s));
+}*/
