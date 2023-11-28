5847be651f2d0f69f6e16c6ea1a21cec5c1a87fb
Maria Luiza Rabelo Serpa <mrabelo-@c0c5c2.42berlin.de>
2023-11-28 17:58:37 +0100

    Correcting ft_split.c

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 16:41:35 by mrabelo-          #+#    #+#             */
-/*   Updated: 2023/11/23 18:37:23 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2023/11/28 17:14:49 by mrabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static size_t	ft_countword(char const*s, char c)
 {
 	size_t	i;
 	size_t	word_count;
+	int		new_word;
 
 	if (s == 0)
 		return (0);
@@ -23,28 +24,89 @@ static size_t	ft_countword(char const*s, char c)
 	word_count = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		new_word = 0;
+		while (s[i] == c && s[i] != '\0')
+			i++;
+		while (s[i] != c && s[i] != '\0')
 		{
-			word_count++;
+			if (new_word == 0)
+			{
+				word_count++;
+				new_word = 1;
+			}
+			i++;
 		}
-		i++
 	}
 	return (word_count);
 }
 
-char	**ft_split(char const*s, char c)
+static size_t	try_malloc(char**dest, size_t pos, size_t size)
 {
 	size_t	i;
-	size_t	j;
+
+	i = 0;
+	dest[pos] = malloc(size);
+	if (dest[pos] == 0)
+	{
+		while (i < pos)
+		{
+			free(dest[i++]);
+		}
+		free(dest);
+		return (1);
+	}
+	return (0);
+}
+
+static size_t	ft_fill_words(char**dest, char const*s, char c)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	while (*s)
+	{
+		len = 0;
+		while (*s == c && *s)
+			s++;
+		while (*s != c && *s)
+		{
+			len++;
+			s++;
+		}
+		if (len)
+		{
+			if (try_malloc(dest, i, len + 1))
+				return (1);
+			ft_strlcpy(dest[i], s - len, len + 1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+char	**ft_split(char const*s, char c)
+{
+	size_t	words;
 	char	**dest;
 
-	dest = (char **)malloc(sizeof(char *) * (ft_countword(s, c) + 1));
+	words = ft_countword(s, c);
+	dest = (char **)malloc(sizeof(char *) * (words + 1));
 	if (s == 0 || dest == 0)
 		return (0);
-	i = 0;
-	j = 0;
-	while (s[i] != '\0')
-	{
-
-	}
+	if (ft_fill_words(dest, s, c))
+		return (0);
+	dest[words] = 0;
+	return (dest);
 }
+
+/*#include <stdio.h>
+
+int	main()
+{
+	char	*s = "   lorem   ipsum dolor     sit amet";
+	char	**v = ft_split(s, ' ');
+
+	while (*v)
+		printf("%s\n", *v++);
+}*/
