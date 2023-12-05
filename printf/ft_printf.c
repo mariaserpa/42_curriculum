dda1a8f1feb95ec7d28d8ea192073b7b51372cd1
Maria Luiza Rabelo Serpa <mrabelo-@c4b4c1.42berlin.de>
2023-12-05 19:58:17 +0100

    Creating unfinished files

---
+++ printf/ft_printf.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 16:09:28 by mrabelo-          #+#    #+#             */
+/*   Updated: 2023/12/05 18:28:48 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_format(const char*str, va_list args, int i)
+{
+	int	len;
+
+	len = 0;
+	if (str[i] == 'c')
+		len += ft_print_char((char) va_arg(args, int));
+	else if (str[i] == 's')
+	{
+		/* code */
+	}
+
+}
+
+
+int	ft_printf(const char*input, ...)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (input[i])
+	{
+		if (input[i] == '%')
+		{
+			len +=
+		}
+		else
+		{
+
+		}
+	}
+}
---
+++ printf/ft_printf.h
@@ -0,0 +1,15 @@
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdarg.h>
+
+int	ft_printf(const char*input, ...);
+
+
+int	ft_print_char(char c);
+
+#endif
