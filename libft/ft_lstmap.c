bce4224de16e77e28a57a268d6fa31722e8b2cc6
Maria Luiza Rabelo Serpa <mrabelo-@c4b4c1.42berlin.de>
2023-12-05 15:12:28 +0100

    Fixing memory leak in ft_lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 14:19:58 by mrabelo-          #+#    #+#             */
-/*   Updated: 2023/11/30 14:24:47 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2023/12/05 15:10:34 by mrabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list*lst, void *(*f)(void*), void (*del)(void*))
 {
 	t_list	*new_lst;
 	t_list	*new_nod;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (0);
 	new_lst = 0;
 	while (lst)
 	{
-		new_nod = ft_lstnew(f(lst -> content));
+		new_content = f(lst -> content);
+		new_nod = ft_lstnew(new_content);
 		if (!new_nod)
 		{
+			del(new_content);
 			ft_lstclear(&new_lst, del);
 			return (0);
 		}
