d11759ecda94d38b3e5157c13f0f8e3fbabb0a5f
Maria Luiza Rabelo Serpa <mrabelo-@c4b7c2.42berlin.de>
2024-05-17 14:40:49 +0200

    Fix header and add norminette rule in Makefile

--- push_swap/checker/checker.h
+++ push_swap/checker/checker.h
@@ -6,7 +6,7 @@
 /*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 19:34:34 by mrabelo-          #+#    #+#             */
-/*   Updated: 2024/05/17 11:31:28 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2024/05/17 14:28:19 by mrabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,28 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-char	*get_next_line(int fd);
-char	*retrieve_line(t_list*lst);
-int		find_newline(t_list*lst);
-int		find_length_line(t_list*lst);
-void	refine_list(t_list**lst);
-void	copy_str(t_list*lst, char*str);
-void	create_list(t_list**lst, int fd);
-void	append_node(t_list**lst, char*buffer);
-void	dealloc(t_list**lst, t_list*new_node, char*buffer);
-
+/*utils*/
 void	start_second_stack(t_stack**b, t_stack**a);
 void	end_stacks(t_stack**a, t_stack**b, bool flag);
 void	run_commands(char*commands, t_stack**a, t_stack**b);
 
+/*checks*/
 void	check_swap(char*commands, t_stack**a, t_stack**b);
 void	check_rotate(char*commands, t_stack**a, t_stack**b);
 void	check_reverse_rotate(char*commands, t_stack**a, t_stack**b);
 void	check_result(t_stack*a, t_stack*b);
 
+/*get_next_line*/
+void	refine_list(t_list**lst);
+char	*retrieve_line(t_list*lst);
+void	append_node(t_list**lst, char*buffer);
+void	create_list(t_list**lst, int fd);
+char	*get_next_line(int fd);
+
+/*get_next_line_utils*/
+int		find_newline(t_list*lst);
+int		find_length_line(t_list*lst);
+void	copy_str(t_list*lst, char*str);
+void	dealloc(t_list**lst, t_list*new_node, char*buffer);
 
 #endif
