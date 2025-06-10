5dce8b8e9c851831daed9b376f979f3ebf4dd137
mariaserpa <iza-serpa@hotmail.com>
2025-06-10 13:29:31 +0200

    add files

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 22:42:42 by mrabelo-          #+#    #+#             */
+/*   Updated: 2025/06/09 22:53:24 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal() : type("Unknown")
+{
+	std::cout << "Animal default constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal &other) : type(other.type)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+}
+
+Animal &Animal::operator=(const Animal &other)
+{
+	std::cout << "Animal assignment operator called" << std::endl;
+	if (this != &other)
+		type = other.type;
+	return *this;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+void Animal::makeSound() const
+{
+	std::cout << "Animal makes a sound" << std::endl;
+}
+
+std::string Animal::getType() const
+{
+	return type;
+}
---
+++ cpp04/ex00/Dog.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 22:57:13 by mrabelo-          #+#    #+#             */
+/*   Updated: 2025/06/09 22:58:14 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+	public:
+		Dog();
+		Dog(const Dog &other);
+		Dog &operator=(const Dog &other);
+		~Dog();
+
+		void makeSound() const;
+};
+
+#endif
