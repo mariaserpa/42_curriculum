3d11419ca80ed0fd57c764cd108d74eb895c2c4e
mariaserpa <iza-serpa@hotmail.com>
2025-10-28 10:39:15 +0100

    update

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 09:53:59 by mrabelo-          #+#    #+#             */
-/*   Updated: 2025/10/28 10:35:52 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2025/10/28 10:38:44 by mrabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int main() {
 	
 	// The :: operator specifies that you want to use the global version of a function/variable, not one from a namespace like std::
 	// Swap integers
-	int a = 5, b = 10;
-	std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
-	swap(a, b);
-	std::cout << "After swap:  a = " << a << ", b = " << b << std::endl;
-	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
-	
+	int e = 5, f = 10;
+	std::cout << "Before swap: e = " << e << ", f = " << f << std::endl;
+	swap(e, f);
+	std::cout << "After swap:  e = " << e << ", f = " << f << std::endl;
+	std::cout << "min(e, f) = " << ::min(e, f) << std::endl;
+	std::cout << "max(e, f) = " << ::max(e, f) << std::endl;
+
 	// Swap doubles
 	double x = 3.14, y = 2.71;
 	std::cout << "Before swap: x = " << x << ", y = " << y << std::endl;
@@ -42,5 +42,22 @@ int main() {
 	std::cout << "After swap:  str1 = " << str1 << ", str2 = " << str2 << std::endl;
 	std::cout << "min(str1, str2) = " << ::min(str1, str2) << std::endl;
 	std::cout << "max(str1, str2) = " << ::max(str1, str2) << std::endl;
+	
+	
+	std::cout << "MAIN from the subject" << std::endl;
+	
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
 	return 0;
 }
